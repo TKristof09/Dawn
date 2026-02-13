4e1080b9cb758a02497142d990f818b469feb6b0
Kristof Toth <kristoft.dev@pm.me>
2026-02-13 17:24:06 +0100

    Bump menhir version

--- lib/parser.ml
+++ lib/parser.ml
@@ -2,10 +2,10 @@ open Lexing
 module I = Menhir_parser.MenhirInterpreter
 
 let get_parse_error env =
-    match I.stack env with
-    | (lazy Nil) -> "Invalid syntax"
-    | (lazy (Cons (I.Element (state, _, _, _), _))) -> (
-        try Parser_messages.message (I.number state) with
+    match I.top env with
+    | None -> "Invalid syntax"
+    | Some (Element (current, _semv, _startp, _endp)) -> (
+        try Parser_messages.message (I.number current) with
         | Not_found -> "invalid syntax (no specific message for this eror)")
 
 let rec parse_aux lexbuf checkpoint =
